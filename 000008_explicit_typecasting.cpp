6101893291810375c94bfaef9734d2b6c5941ce6
Akash Kumar <akashkumar888333@gmail.com>
2026-09-19 19:30:36 +0530

    000008_explicit_typecasting.cpp

---
+++ 000008_explicit_typecasting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+   int main()
+{
+   int a=9;
+   double a2=(double)a;
+   cout<<a2<<endl;
+   double b=5.5;
+   int b2=(int)b;
+   cout<<b2<<endl;
+   return 0;
+}
